bc4c08566988b538964659aa3c39472cd72e4b49
lindatrosken <linda.troesken@gmx.net>
2021-07-20 19:52:08 +0200

    create StudentDB class and StudentDBTest class with list, toString, equals

--- 21-7-20/src/main/java/de/neuefische/objects/StudentDB.java
+++ 21-7-20/src/main/java/de/neuefische/objects/StudentDB.java
@@ -1,4 +1,38 @@
 package de.neuefische.objects;
 
+import java.util.Arrays;
+
 public class StudentDB {
+
+    private Student[] students;
+
+    public StudentDB(Student[] students) {
+        this.students = students;
+    }
+
+    public Student[] listStudents(){
+        return students;
+    }
+
+    @Override
+    public String toString() {
+        String returnString = "";
+        for (int i = 0; i < students.length; i++) {
+            returnString += students[i].toString() +"\n";
+        }
+        return returnString;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StudentDB studentDB = (StudentDB) o;
+        return Arrays.equals(students, studentDB.students);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(students);
+    }
 }
--- 21-7-20/src/test/java/de/neuefische/objects/StudentDBTest.java
+++ 21-7-20/src/test/java/de/neuefische/objects/StudentDBTest.java
@@ -1,7 +1,47 @@
 package de.neuefische.objects;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentDBTest {
+
+    @Test
+    @DisplayName("Test list method")
+    public void testList(){
+        // GIVEN
+        Student student1 = new Student("Erika", 123);
+        Student student2 = new Student("Max", 234);
+        Student[] students = {student1, student2};
+        StudentDB studentDB = new StudentDB(students);
+
+        // THEN
+        Student[] actualStudents = studentDB.listStudents();
+
+        // WHEN
+        Student[] expectedStudents = {student1, student2};
+        assertArrayEquals(expectedStudents, actualStudents);
+
+    }
+
+    @Test
+    @DisplayName("Test toString method")
+    public void testToString(){
+        // GIVEN
+        Student student1 = new Student("Erika", 123);
+        Student student2 = new Student("Max", 234);
+        Student[] students = {student1, student2};
+        StudentDB studentDB = new StudentDB(students);
+
+        // THEN
+        String actualStudentsString = studentDB.toString();
+
+        // WHEN
+        String expectedStudentsString = "Name: Erika, ID: 123\nName: Max, ID: 234\n";
+        assertEquals(expectedStudentsString, actualStudentsString);
+
+    }
+
 
 }
